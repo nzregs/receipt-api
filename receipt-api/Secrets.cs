7c3d43f64b386379f2a43d6f5c432bc7aa67ef6f
nzregs <regan_murphy@hotmail.com>
2017-10-20 14:43:31 +1300

    Update Secrets.cs

--- receipt-api/Secrets.cs
+++ receipt-api/Secrets.cs
@@ -7,7 +7,7 @@ namespace receipt_api
 {
     public class Secrets
     {
-        public const string apikey = "f4c2f91d0b704d3f814158c6a4b23dd6";
+        public const string apikey = "f4c2@@@@@@@@@@@@@@@@@@@@@@@@3dd6";
         public const string apiendpoint_ocr = @"https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
 
     }
